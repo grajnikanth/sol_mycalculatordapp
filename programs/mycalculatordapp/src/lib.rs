54817c9ef11ba698d12bc060c362d18631d97d35
Raj Gedhada <grajnikanth@gmail.com>
2022-10-09 16:17:40 -0700

    Added Create method to Program and corresponding tests to client

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -6,10 +6,38 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod mycalculatordapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>, init_message: String) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
         Ok(())
     }
 }
 
+// Context - list of accounts create function needs to read from to execute it's logic
+// Solana accounts are separate from smart contract
+
+/**
+ *  Below Create struct contains the three accounts needed for the create 
+ *  instruction/function to work. The three accounts are:
+ *          calculator, user and system_program
+ *  
+ */
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space=264)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+/**
+ *  Calculator struct defines what data we will store in an instance of Calculator
+ *  account. 
+ */
+#[account]
+pub struct Calculator {
+    pub greeting: String,       // ??? bytes
+    pub result: i64,            // 8 bytes
+    pub remainder: i64          // 8 bytes
+}
--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -1,16 +1,38 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Mycalculatordapp } from "../target/types/mycalculatordapp";
+const assert = require('assert')
+const anchor = require('@project-serum/anchor')
+const {SystemProgram} = anchor.web3
 
-describe("mycalculatordapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe('mycalculatordapp', () => {
 
-  const program = anchor.workspace.Mycalculatordapp as Program<Mycalculatordapp>;
+    const provider = anchor.AnchorProvider.local();
+    console.log("Provider properties based on local method");
+    console.log("provider.connection is ", provider.connection);
+    console.log("provider.publicKey is ", provider.publicKey.toString());
+    console.log("provider.wallet is ", provider.wallet);
+    console.log("provider.wallet.publicKey is ", provider.wallet.publicKey.toString());
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    anchor.setProvider(provider)
+    const calculator = anchor.web3.Keypair.generate()
+    const program = anchor.workspace.Mycalculatordapp
+    console.log("program variable - ", program);
+    
+
+    it('creates a calculator', async() => {
+        await program.rpc.create("welcome to calculator", {
+            accounts: {
+                calculator: calculator.publicKey,
+                user: provider.wallet.publicKey,
+                systemProgram: SystemProgram.programId
+            },
+            signers: [calculator]
+        })
+
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        console.log("calculator account data obtained from Blockchain");
+        console.log("fetch function of anchor returns deserialized account");
+        console.log("account.greeting is ", account.greeting);
+        console.log("account.result is ", account.result);
+        console.log("account.remainder is ", account.remainder);
+        assert.ok(account.greeting == "welcome to calculator")
+    })
+})
